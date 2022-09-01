6c7df58f01693aafa719704c138c1e73cd470e02
Алексей Никитченко <achestron@gmail.com>
2022-09-01 12:32:07 +0300

    Редактирует

--- js/jaba-cipher.js
+++ js/jaba-cipher.js
@@ -1,28 +1,59 @@
-function jabaCipher(str) {
-  const vowel = new RegExp(/^[аеёиоуыэюя]+[^:,?!_()]+$/, "i"); //regexp for vowels
-  const consonant = new RegExp(/^[бвгджзйклмнпрстфхцчшщ]+[^:,?!_()]+$/, "i"); //regexp for consonants
-  let wordByWordStr = str.split(/[\s]/); //divide the string word by word
-  for (let i = 0; i < wordByWordStr.length; i++) { //adding the jaba last word parts
-    if (vowel.test(wordByWordStr[i])) {
-      wordByWordStr[i] = wordByWordStr[i] + "уэкэ";
-    } else if (consonant.test(wordByWordStr[i])) {
-        let LastPartOfWord = wordByWordStr[i].match(/^[бвгджзйклмнпрстфхцчшщ]+/i); 
-        wordByWordStr[i] = wordByWordStr[i].replace(/^[бвгджзйклмнпрстфхцчшщ]+/i, "аэкэ") + LastPartOfWord + "рэкэ"; //confusing words with consonants
+let inputField = document.getElementById('cipher-input'); //получаем текст
+let outputField = document.getElementById('cipher-output'); //выводим шифрованный текст
+const vowelFirst = new RegExp('^[аеёиоуыэюя]+[^:,?!_()]+$', 'i'); //регэксп для слов на гласные (но без спецсимволов)
+const consonantFirst = new RegExp('^[бвгджзйклмнпрстфхцчшщ]+[^:,?!_()]+$', 'i'); //регэксп для слов на согласные (также без спецсимволов)
+const exVowel = new RegExp('(уэкэ)$', ''); //регэксп для шифрованных гласных
+const exConsonant = new RegExp('^(аэкэ)', ''); //регэксп для шифрованных согласных
+const cipherAttribute = new RegExp('🐸+', 'g'); //регэксп для распознавания шифрованного текста
+
+inputField.addEventListener('keyup', function() { //проверяем какой текст получили и либо шифруем, либо дешифруем
+  if (!cipherAttribute.test(inputField.value)) {
+    outputField.innerText = makejabaCipher(inputField.value);
+  } else {
+    outputField.innerText = decipherer(inputField.value);
+  }
+});
+
+function makejabaCipher(str) {
+  let wordByWordStr = str.split(/\s/); //разбивает предложение пословно
+
+  for (let i = 0; i < wordByWordStr.length; i++) {
+    if (vowelFirst.test(wordByWordStr[i])) { //добавляет окончание гласным
+      wordByWordStr[i] = wordByWordStr[i] + 'уэкэ';
+    } else if (consonantFirst.test(wordByWordStr[i])) { //добавляет начало и окончание согласным и переставляет букву
+        let LastletterOfWord = wordByWordStr[i].match(/^[бвгджзйклмнпрстфхцчшщ]{1}/i);
+        wordByWordStr[i] = wordByWordStr[i].replace(/^[бвгджзйклмнпрстфхцчшщ]{1}/i, 'аэкэ') + LastletterOfWord + 'рэкэ'; //confusing words with consonants
     }
   }
-  let randomNum = Math.floor(Math.random() * (wordByWordStr.length - 1)) + 1;
-  if (Math.floor(wordByWordStr.length / 3) >= 1) { //adding the jaba emodzi throw random places
-    for (let j = 0; j < randomNum; j++) {
-      let randomJabs = Math.floor(Math.random() * (wordByWordStr.length));
-      wordByWordStr[randomJabs] = wordByWordStr[randomJabs] + " 🐸";
-    }
+
+  let randomNum = Math.floor(Math.random() * (wordByWordStr.length - 1) + 1);
+
+  for (let j = 0; j < randomNum; j++) { //ставит эмодзи жаб в рандомные места
+    let randomJabsNumber = Math.floor(Math.random() * (wordByWordStr.length));
+    wordByWordStr[randomJabsNumber] = wordByWordStr[randomJabsNumber] + ' 🐸';
   }
-  return wordByWordStr.join(" "); //return JabaCipher
+
+  return wordByWordStr.join(' '); //return JabaCipher
 };
 
-let str = document.getElementById("cipher-input"); //get input text from textarea
-let cipherStr = document.getElementById("cipher-output"); //get output text field
+function decipherer(str) {
+  let wordByWordStr = str.split(/\s/).filter(item => item !== '🐸'); //разбивает строку пословно и фильтрует от эмодзи
+  let decipheredStr = [];
 
-str.addEventListener("keypress", function() { //creating text inside <p> tag
-  cipherStr.innerText = jabaCipher(str.value);
-});
+  for (let word of wordByWordStr) {
+    switch(true) {
+      case exVowel.test(word): //дешифрует бывшие слова на гласные
+        decipheredStr.push(word.replace(/уэкэ$/, ''));
+        break;
+      case exConsonant.test(word): //дешифрует бывшие слова на согласные
+        let newWord = word.replace(/^аэкэ/, '').replace(/рэкэ$/, '');
+        let LastletterOfDeciphererWord= newWord.match(/[бвгджзйклмнпрстфхцчшщ]{1}$/i);
+        decipheredStr.push(LastletterOfDeciphererWord + newWord.replace(/[бвгджзйклмнпрстфхцчшщ]{1}$/i, ''));
+        break;
+      default: //не трогает слова со спецсимволами и союзы
+        decipheredStr.push(word);
+    }
+  }
+
+  return decipheredStr.join(' ');
+};
